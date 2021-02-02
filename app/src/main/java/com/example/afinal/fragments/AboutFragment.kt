abecfc95bed4233734559912127b847ef8791fd8
Beckauri <beka.bekauri.3@btu.edu.ge>
2021-02-02 08:56:50 +0400

    'new commit'

--- app/src/main/java/com/example/afinal/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/afinal/fragments/AboutFragment.kt
@@ -1,7 +1,25 @@
 package com.example.afinal.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.afinal.R
 
 class AboutFragment : Fragment(R.layout.fragment_about) {
+
+    private lateinit var editText: EditText
+    private lateinit var navController: NavController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        navController = Navigation.findNavController(view)
+
+        editText = view.findViewById(R.id.textView)
+    }
 }
--- app/src/main/java/com/example/afinal/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/afinal/fragments/SettingsFragment.kt
@@ -1,7 +1,33 @@
 package com.example.afinal.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.afinal.R
 
 class SettingsFragment : Fragment(R.layout.activity_change_password) {
+
+    private lateinit var navController: NavController
+    private lateinit var editText: EditText
+    private lateinit var button:Button
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        editText = view.findViewById(R.id.NewPasswordEditText)
+        button = view.findViewById(R.id.NewPasswordSubmit)
+
+        button.setOnClickListener {
+            var amount = editText.toString().toInt()
+
+
+        }
+    }
 }
